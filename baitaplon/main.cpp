4ae207d20457859e1cbd8fd2a33eafa4912dcf91
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2022-05-13 00:51:33 +0700

    .

--- baitaplon/main.cpp
+++ baitaplon/main.cpp
@@ -14,11 +14,13 @@ SDL_Window *window;
 Plane player;
 Plane bullet;
 Plane enemy;
+Plane enemybullet;
 game app;
 int score;
+int highscore;
+int damage=0;
 bool enter=false;
 void Screen() {
-//    SDL_SetRenderDrawColor(renderer,96,128 ,255,255);
     SDL_RenderClear(renderer);
 }
 void upScence() {
@@ -44,8 +46,25 @@ void blit(SDL_Texture *texture, int x, int y) {
 }
 
 void doKeyDown(SDL_KeyboardEvent *event) {
-    if(event->keysym.scancode==SDL_SCANCODE_SPACE){
+    if(event->keysym.scancode==SDL_SCANCODE_E){
+        damage=1;
+        score=0;
+        player.health=5;
         enter=true;
+    }
+    if(event->keysym.scancode==SDL_SCANCODE_M){
+        damage=2;
+        score =0;
+        player.health=4;
+        enter=true;
+    }
+    if(event->keysym.scancode==SDL_SCANCODE_H){
+        damage=3;
+        score =0;
+        player.health=3;
+        enter=true;
+    }if(event->keysym.scancode==SDL_SCANCODE_R){
+        enter=false;
     }
     if (event->repeat == 0) {
         if (event->keysym.scancode == SDL_SCANCODE_UP) {
@@ -68,12 +87,23 @@ void doKeyDown(SDL_KeyboardEvent *event) {
         }
         if(event->keysym.scancode== SDL_SCANCODE_X) {
             quitSDL(window,renderer);
-            cerr<<score;
         }
     }
 }
 void doKeyUp(SDL_KeyboardEvent *event) {
-    if(event->keysym.scancode==SDL_SCANCODE_SPACE){
+    if(event->keysym.scancode==SDL_SCANCODE_E){
+        damage=1;
+        player.health=5;
+        enter=true;
+    }
+    if(event->keysym.scancode==SDL_SCANCODE_M){
+        damage=2;
+        player.health=4;
+        enter=true;
+    }
+    if(event->keysym.scancode==SDL_SCANCODE_H){
+        damage=3;
+        player.health=3;
         enter=true;
     }
     if (event->repeat == 0) {
@@ -97,7 +127,6 @@ void doKeyUp(SDL_KeyboardEvent *event) {
         }
         if(event->keysym.scancode== SDL_SCANCODE_X) {
             quitSDL(window,renderer);
-            cerr<<score;
         }
     }
 }
@@ -179,60 +208,46 @@ SDL_Texture *getTextTexture(const char *text)
 {
 	SDL_Surface *surface;
 
-	surface = TTF_RenderText_Solid(TTF_OpenFont("font.ttf",25), text,WHITE_COLOR);
+	surface = TTF_RenderText_Solid(TTF_OpenFont("font.ttf",20), text,WHITE_COLOR);
 
 	return toTexture(surface, 1);
 }
 void screenscore(){
-    string s="Score: ";
+    string s="Score:";
     s+=to_string(score);
     SDL_Texture* point= getTextTexture(s.c_str());
     blit(point,10,10);
+    string w="Hi:";
+    w+=to_string(highscore);
+    SDL_Texture* hipoint= getTextTexture(w.c_str());
+    blit(hipoint,SCREEN_WIDTH-100,10);
 }
-
-int main(int argc,char* argv[]) {
-    IMG_Init(IMG_INIT_PNG);
-    TTF_Init();
-    SDL_Init(SDL_INIT_AUDIO);
-    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY*2,MIX_DEFAULT_FORMAT,MIX_DEFAULT_CHANNELS,2048);
-    Mix_Music *bgm= Mix_LoadMUS("bgm.mp3");
-    initSDL(window,renderer,WINDOW_TITLE,SCREEN_WIDTH,SCREEN_HEIGHT);
-    player.x = 100;
-    player.y = 100;
-    player.texture = loadTexture("player.png");
-    bullet.texture = loadTexture("bullet.png");
-    enemy.texture= loadTexture("enemy.png");
-    SDL_Texture *backgr=loadTexture("backgrnew.png");
-    SDL_Texture* explos= loadTexture("explosion.png");
-    spawnenemy();
-    Mix_PlayMusic(bgm,-1);
-    while(true) {
-        doInput();
-        if(enter) {
-            Screen();
-            blit(backgr,0,0);
-            screenscore();
-            setpos(player);
-            setpos(enemy);
-            setpos(bullet);
-            if(collision(bullet.x,bullet.y,bullet.w,bullet.h,enemy.x,enemy.y,enemy.w,enemy.h)) {
-                blit(explos,enemy.x,enemy.y);
-                spawnenemy();
-                score++;
-                screenscore();
-            }
-            blit(enemy.texture,enemy.x,enemy.y);
-            if(player.x>SCREEN_WIDTH/2-70) {
+void enemyfire(){
+    if (enemybullet.health == 0) {
+        enemybullet.x = enemy.x;
+        enemybullet.y = enemy.y+24;
+        enemybullet.health = 1;
+    }
+    enemybullet.x -= BULLET_SPEED;
+    if (enemybullet.x <0||collision(enemybullet.x,enemybullet.y,enemybullet.w,enemybullet.h,player.x,player.y,player.w,player.h)) {
+        enemybullet.health = 0;
+    }
+    if (enemybullet.health > 0) {
+        blit(enemybullet.texture, enemybullet.x, enemybullet.y);
+    }
+}
+void playermove(){
+    if(player.x>SCREEN_WIDTH/2-70) {
                 player.x=SCREEN_WIDTH/2-70;
             }
             if(player.x<0) {
                 player.x=0;
             }
-            if(player.y<0) {
-                player.y=0;
+            if(player.y<20) {
+                player.y=20;
             }
-            if(player.y>SCREEN_HEIGHT-player.h+20) {
-                player.y=SCREEN_HEIGHT-player.h;
+            if(player.y>SCREEN_HEIGHT-player.h-20) {
+                player.y=SCREEN_HEIGHT-player.h-20;
             }
 
             if (app.up) {
@@ -250,19 +265,95 @@ int main(int argc,char* argv[]) {
             if (app.right) {
                 player.x += PLAYER_SPEED;
             }
+}
+void Menu()
+{
+    SDL_Texture *s1=getTextTexture("Choose game levels:");
+    SDL_Texture *s2=getTextTexture("Press 'E' for easy");
+    SDL_Texture *s3=getTextTexture("Press 'M' for medium");
+    SDL_Texture *s4=getTextTexture("Press 'H' for hard");
+    blit(s1,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2-100);
+    blit(s2,SCREEN_WIDTH/2-90,SCREEN_HEIGHT/2-70);
+    blit(s3,SCREEN_WIDTH/2-90,SCREEN_HEIGHT/2-40);
+    blit(s4,SCREEN_WIDTH/2-90,SCREEN_HEIGHT/2-10);
+    SDL_Texture *s5=getTextTexture("Press 'X' to exit");
+    blit(s5,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2+20);
 
+}
+void gameover(){
+    SDL_Texture*g1= getTextTexture("Press 'R' to play again");
+    SDL_Texture*g2= getTextTexture("Press 'X' to exit");
+    SDL_Texture *g3= getTextTexture("You Died!");
+    string s="Your score:";
+    s+=to_string(score);
+    SDL_Texture*g4= getTextTexture(s.c_str());
+    blit(g4,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2-70);
+    blit(g1,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2-40);
+    blit(g2,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2-10);
+    blit(g3,SCREEN_WIDTH/2-150,SCREEN_HEIGHT/2-100);
+}
+
+int main(int argc,char* argv[]) {
+    IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_OpenAudio(MIX_DEFAULT_FREQUENCY*2,MIX_DEFAULT_FORMAT,MIX_DEFAULT_CHANNELS,2048);
+    Mix_Music *bgm= Mix_LoadMUS("bgm.mp3");
+    initSDL(window,renderer,WINDOW_TITLE,SCREEN_WIDTH,SCREEN_HEIGHT);
+    player.x = 100;
+    player.y = 100;
+    player.texture = loadTexture("player.png");
+    bullet.texture = loadTexture("bullet.png");
+    enemy.texture= loadTexture("enemy.png");
+    enemybullet.texture=loadTexture("enemybullet.1.png");
+    SDL_Texture *backgr=loadTexture("backgrnew.png");
+    SDL_Texture* explos= loadTexture("explosion.png");
+    spawnenemy();
+    Mix_PlayMusic(bgm,-1);
+    while(true) {
+        doInput();
+        if(enter) {
+            Screen();
+            blit(backgr,0,0);
+            screenscore();
+            setpos(player);
+            setpos(enemy);
+            setpos(bullet);
+            if(collision(bullet.x,bullet.y,bullet.w,bullet.h,enemy.x,enemy.y,enemy.w,enemy.h)) {
+                blit(explos,enemy.x,enemy.y);
+                spawnenemy();
+                enemybullet.health=0;
+                score++;
+                if(highscore<score){
+                    highscore++;
+                }
+                screenscore();
+            }
+            if(collision(enemybullet.x,enemybullet.y,enemybullet.w,enemybullet.h,player.x,player.y,player.w,player.h)){
+                player.health-=damage;
+            }
+            blit(enemy.texture,enemy.x,enemy.y);
+            enemyfire();
+            playermove();
+            string s="Health:";
+            s+=to_string(player.health);
+            SDL_Texture* health=getTextTexture(s.c_str());
+            blit(health,SCREEN_WIDTH/2,10);
             blit(player.texture,player.x,player.y);
+            if(player.health<=0){
+                blit(backgr,0,0);
+                app.fire=0;
+                gameover();
+            }
             dobullet();
             upScence();
             SDL_Delay(16);
         } else {
             Screen();
-            SDL_Texture* font= getTextTexture("Press 'Space' to start ");
             blit(backgr,0,0);
-            blit(font,SCREEN_HEIGHT/2,SCREEN_WIDTH/2);
+            Menu();
             upScence();
         }
-
     }
     return 0;
 }
